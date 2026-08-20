039b7c49af213c9bbe3b9d6026ae6de5c9ff49a4
ojaswitameena06 <ojaswitameena06@users.noreply.github.com>
2026-08-20 03:50:07 +0000

    deploy: beebf7cde1540b61d20686e3dc414b6184c93a73

--- main.dart.js
+++ main.dart.js
@@ -30352,7 +30352,7 @@ $S:2}
 A.aBv.prototype={
 $1(a){var s=A.df().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/5f77625673248ee5846fbcaf5d3e1a3878386fd7/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5d531788691ec3404cac0cee66ead4007b177363/":s)+a},
 $S:65}
 A.P4.prototype={
 gv(a){var s=this.a
